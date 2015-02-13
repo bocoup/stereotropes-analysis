7be2023184d9971329d957d9d20a2268fc342e0b
Irene Ros <imirene@gmail.com>
2015-02-13 16:06:23 -0500

    weird diff issue

--- analyse_data.py
+++ analyse_data.py
@@ -52,13 +52,8 @@ def compute_LL(source_corpus, background_corpus):
         ll = loglike(count, source_len, background_freq[word], bg_len)
         results.append((word, count, ll))
 
-<<<<<<< HEAD
     sorted_ll = sorted(results, key=lambda tup: tup[2], reverse=True)
-    pp(sorted_ll[0:20])
-=======
-	sorted_ll = sorted(results, key=lambda tup: tup[2], reverse=True)
-	# pp(sorted_ll[0:20])
->>>>>>> 33dcafe8cba89c0101d94833d06ff54dd9593f86
+    # pp(sorted_ll[0:20])
 
     return sorted_ll
 
