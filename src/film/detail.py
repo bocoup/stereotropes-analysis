3df023d24f490f3d84461b89edb7137f17efbda0
Irene Ros <imirene@gmail.com>
2015-02-18 17:11:47 -0500

    add film detail extraction

--- src/film/detail.py
+++ src/film/detail.py
@@ -1,12 +1,15 @@
-import requests
-import requests_cache
-from os.path import splitext, basename, join
-import time
-import json
-import math
-import wget
+# Builds detail files for each film
+#
+# python -m src.film.detail --src=data/results/films_with_poster_files.json
+# --dest=/Users/iros/dev/bocoup/tvtropes/data/films/detail
+# --roles data/results/film_roles-female.json
+# --roles data/results/film_roles-male.json
+# --extended=True
+#
+
+from os.path import join
 from src import util
-from src.film import util as filmutil
+from src.film import roles
 
 def get_film_base(film):
   base = {
@@ -30,7 +33,7 @@ def get_film_extendeed(film):
   else:
     return None
 
-def extract_detail_files(films, dest, name=None, extended=False):
+def extract_detail_files(films, dest, roles, name=None, extended=False):
 
   for film in films:
     write = True
@@ -44,8 +47,12 @@ def extract_detail_files(films, dest, name=None, extended=False):
       else:
         data = get_film_base(film)
 
-      if data:
-        print data['name']
+      if roles and data:
+        # Extract role data
+        data['roles'] = { 'm' : None, 'f' : None }
+        if film['id'] in roles:
+          data['roles'] = roles[film['id']]
+
         util.write_json(join(dest, data['id'] + '.json'), data)
 
 if __name__ == "__main__":
@@ -53,6 +60,7 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Generate the info dict for tropes')
     parser.add_argument('--src', help='Source films dictionary', required=True)
+    parser.add_argument('--roles', help='Film role files for both genders', required=False, action='append')
     parser.add_argument('--dest', help='Destination folder', required=True)
     parser.add_argument('--extended', help='If true, will output extended form', required=False)
     parser.add_argument('--name', help='Specific movie to generate information on', required=False)
@@ -61,4 +69,8 @@ if __name__ == "__main__":
 
     films = util.read_json(args.src)
 
-    extract_detail_files(films, args.dest, args.name, args.extended)
+    movie_roles = None
+    if args.roles:
+      movie_roles = roles.get_roles(args.roles)
+
+    extract_detail_files(films, args.dest, movie_roles, args.name, args.extended)
---
+++ src/film/roles.py
@@ -0,0 +1,61 @@
+from src import util
+import tagger
+
+def get_roles(trope_files):
+  ''' Returns the male and female roles identified in this movie'''
+
+  movie_tropes = {}
+  movie_trope_dictionary = {}
+
+  for trope_file in trope_files:
+
+    tropes = util.read_json(trope_file)
+
+    # determine gender based on trope.
+    gender = 'm'
+    if 'female' in trope_file:
+      gender = 'f'
+
+    print "Processing " + gender
+
+    # build map:
+    # movieId -> [trope1, trope2, trope3]
+    for idx, tup in enumerate(tropes):
+      if idx % 100 == 0:
+        print str(idx) + " out of " + str(len(tropes))
+
+      trope = tup[0]
+      film_id = tup[1]
+      role = tup[3]
+
+      if film_id not in movie_tropes:
+        movie_tropes[film_id] = {}
+        movie_trope_dictionary[film_id] = {}
+
+      if gender not in movie_tropes[film_id]:
+        movie_tropes[film_id][gender] = []
+        movie_trope_dictionary[film_id][gender] = {}
+
+      if (
+          # if we haven't seen this trope appear yet for this film
+          trope not in movie_trope_dictionary[film_id][gender] or
+
+          # or, we've seen this trope but the description is different
+          (
+            trope in  movie_trope_dictionary[film_id][gender] and
+            role not in movie_trope_dictionary[film_id][gender][trope]
+          )
+        ):
+        tags = tagger.tag(role)
+        adjectives = []
+        if 'JJ' in tags['by_tag']:
+          adjectives = tags['by_tag']['JJ']
+
+        movie_tropes[film_id][gender].append([trope, role, adjectives])
+
+        if trope in movie_trope_dictionary[film_id][gender]:
+          movie_trope_dictionary[film_id][gender][trope].append(role)
+        else:
+          movie_trope_dictionary[film_id][gender][trope] = [role]
+
+  return movie_tropes
